b67d4a03f9a2c974b55d62107bcd850f764568d8
Lee Jaebeom <woqja125@gmail.com>
2018-05-15 13:59:11 +0900

    remove unused function

--- frontend/src/store/cosnu/sagas.js
+++ frontend/src/store/cosnu/sagas.js
@@ -2,22 +2,6 @@ import { takeEvery, put, call, fork } from 'redux-saga/effects'
 import api from 'services/api'
 import * as actions from './actions'
 
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 export function* watchValidateToken(action){
     const {token} = action
     const response = yield call (fetch, '/api/user/', {

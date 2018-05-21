f5ee139f83f5e7b4cad0fcb72c8d2428ec47a4eb
Lee Jaebeom <woqja125@gmail.com>
2018-05-22 03:44:00 +0900

    edit test

--- frontend/src/components/organisms/LectureList/index.test.js
+++ frontend/src/components/organisms/LectureList/index.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import LectureList from '.'
 
-const wrap = (props = {}) => shallow(<LectureList {...props} />)
+const wrap = (props = {}) => shallow(<LectureList {...props} user_state={{username:"hi", lectures:[]}} />)
 
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })

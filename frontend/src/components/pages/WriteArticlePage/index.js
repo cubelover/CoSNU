3c91b21d5b38ba44abc6fe0da0b1133f2922ff45
cubelover <jeehak0210@gmail.com>
2018-05-28 23:25:00 +0900

    WriteArticlePage

--- frontend/src/components/pages/WriteArticlePage/index.js
+++ frontend/src/components/pages/WriteArticlePage/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import styled, { css } from 'styled-components'
 import { PageTemplate } from 'components'
-import { Input, Button } from 'components'
+import { Input, Button, Textarea } from 'components'
 import { Link } from 'react-router'
+
+const Wrapper = styled.div`
+`
 
 const WriteArticlePage = ({params, action_post_article, children, ...props}) => {
   let title, contents;
@@ -16,9 +19,11 @@ const WriteArticlePage = ({params, action_post_article, children, ...props}) => 
   return (
     <PageTemplate>
       <h1>WriteArticlePage</h1>
-      <Input type="title" placeholder="title" innerRef={(ref) => {title = ref;}}></Input>
-      <Input type="contents" placeholder="contents" innerRef={(ref) => {contents = ref;}}></Input>
-      <Button onClick={post_article}>POST</Button> 
+      <Wrapper>
+        <Input type="text" placeholder="title" innerRef={(ref) => {title = ref;}}></Input>
+        <Button onClick={post_article}>POST</Button>
+      </Wrapper>
+      <Textarea type="contents" placeholder="contents" innerRef={(ref) => {contents = ref;}}></Textarea>
       {children}
     </PageTemplate>
   )

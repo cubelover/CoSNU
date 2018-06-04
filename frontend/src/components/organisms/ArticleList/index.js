15c67ef0daa39f7a455668fa73253c4c06aa3c72
kajebiii <kajebiii@gmail.com>
2018-06-05 04:05:08 +0900

    add (first / prev / pages numbers / next / last) below article table

--- frontend/src/components/organisms/ArticleList/index.js
+++ frontend/src/components/organisms/ArticleList/index.js
@@ -15,7 +15,6 @@ class ArticleList extends React.Component {
     super(props)
   }
   componentDidMount(){
-    console.log(this.props.params)
     this.props.get_articles(this.props.lecture_id, this.props.cur_page)
   }
   componentWillReceiveProps(nextProps) {
@@ -24,8 +23,9 @@ class ArticleList extends React.Component {
   }
   render(){
     var {lecture_id, cur_page, children, articles, location, ...props} = this.props
-    console.log(articles);
-    console.log(location);
+    
+    var article_per_page = 10, max_page_num = Math.floor((articles.count + article_per_page - 1) / article_per_page)
+    var show_page_number = 10, page_base = Math.floor((Math.max(1, cur_page) - 1) / show_page_number) * show_page_number + 1;
     return (
       <Wrapper {...props}>
         <h3>Article List (Lecture_id = {lecture_id})</h3>
@@ -34,8 +34,18 @@ class ArticleList extends React.Component {
         <Link to={{pathname : '/lecture/' + lecture_id + '/list/'}}><Button>List</Button></Link>
         <Link to={{pathname : '/lecture/' + lecture_id + '/write/'}}><Button>Write</Button></Link>
         <ArticleTable articles={articles.results} lecture_id={lecture_id} page={cur_page}/>
-        <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: cur_page-1}}}><Button>Prev</Button></Link>
-        <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: cur_page+1}}}><Button>Next</Button></Link>
+        <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: 1}}}><Button>First Page</Button></Link>
+        {cur_page > 1 && <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: cur_page-1}}}><Button>Prev</Button></Link>}
+        {Array.from(new Array(show_page_number), (val,index)=>page_base+index).map( (page) => {
+          if(page == cur_page) {
+            return <Link to={{ pathname: "/lecture/" + lecture_id + '/list', query: { page: page } }} style={{color: 'red', padding: '5'}} key={page}>{page}</Link>
+          }else if(page <= max_page_num) {
+            return <Link to={{ pathname: "/lecture/" + lecture_id + '/list', query: { page: page } }} style={{padding: '5'}} key={page}>{page}</Link>
+          }
+        }
+        )}
+        {cur_page < max_page_num && <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: cur_page+1}}}><Button>Next</Button></Link>}
+        <Link to={{pathname : '/lecture/' + lecture_id + '/list/', query: {page: max_page_num}}}><Button>Last Page</Button></Link>
         {children}
       </Wrapper>
     )
--- frontend/src/components/pages/LecturePage/index.js
+++ frontend/src/components/pages/LecturePage/index.js
@@ -7,7 +7,6 @@ const LecturePage = ({ params, location, children, ...props}) => {
   let lecture_id = params.lecture_id;
   let cur_page = (location.query.page ? parseInt(location.query.page, 10) : 1);
   if(isNaN(cur_page)) cur_page = 1;
-  console.log("cur_page", cur_page);
   return (
     <PageTemplate>
       <h1>LecturePage {lecture_id}</h1>

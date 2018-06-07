c163f7243a420c24f4209a79b88976fdff2d11e7
Sunghyeon Jo <ainta@naver.com>
2018-06-08 03:26:12 +0900

    make up/downvote

--- frontend/src/components/pages/ArticlePage/index.js
+++ frontend/src/components/pages/ArticlePage/index.js
@@ -20,7 +20,7 @@ class ArticlePage extends React.Component {
     this.props.get_article(nextProps.params.lecture_id, nextProps.params.article_id)
   }
   render(){
-    var {location, action_delete_article, action_post_comment, children, article, ...props} = this.props
+    var {location, action_delete_article, action_post_comment, action_post_upvote, action_post_downvote, children, article, ...props} = this.props
     var {lecture_id, article_id} = this.props.params;
     let cur_page = 1//query.page ? parseInt(query.page, 10) : 1
     if(isNaN(cur_page)) cur_page = 1
@@ -34,14 +34,24 @@ class ArticlePage extends React.Component {
       action_post_comment(lecture_id, article_id, input_comment.value)
       input_comment.value = ''
     }
+    const post_upvote = () => {
+      action_post_upvote(lecture_id, article_id)
+    }
+    const post_downvote = () => {
+      action_post_downvote(lecture_id, article_id)
+    }
     return (
       <PageTemplate>
         <h1>ArticlePage {lecture_id} + {article_id}</h1>
         <span>{article.title}</span>
         <span>{article.author}</span>
         <span>{article.create_time}</span>
+        <span>{article.upvotes}</span>
+        <span>{article.downvotes}</span>
         <p>{article.contents}</p>
         <Button>Modify(todo)</Button>
+        <Button onClick = {post_upvote}>Upvote</Button>
+        <Button onClick = {post_downvote}>Downvote</Button>
         <Link to={{ pathname: '/lecture/' + lecture_id + '/list/' }}><Button onClick = {delete_article}>Delete</Button></Link>
         <Input type="text"></Input>
         <Button>Report(todo)</Button>

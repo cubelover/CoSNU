2365bfb1ba65d88e8f343e5b515effb5072bddf4
kajebiii <kajebiii@gmail.com>
2018-06-11 20:14:54 +0900

    pass jest

--- frontend/src/components/organisms/SearchLectureList/index.test.js
+++ frontend/src/components/organisms/SearchLectureList/index.test.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SearchLectureList from '.'
 
-const wrap = (props = {}) => shallow(<SearchLectureList {...props} lectures={[]}/>)
+const wrap = (props = {}) => shallow(<SearchLectureList 
+  location={{query:{}}} 
+  lectures={[]}
+  {...props} 
+/>)
 
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })

87f1e3d7aa308436dec0d7c5659cfbb497f1cb5e
kajebiii <kajebiii@gmail.com>
2018-06-11 23:58:12 +0900

    pass jest

--- frontend/src/components/molecules/EditLectureTable/index.js
+++ frontend/src/components/molecules/EditLectureTable/index.js
@@ -8,33 +8,56 @@ const Wrapper = styled.div`
   color: ${palette('grayscale', 0)};
 `
 
-const EditLectureTable = ({lectures, children, ...props}) => {
-  console.log("hihiihihi", lectures);
-  return (
-    <Wrapper {...props}>
-      <Table>
-        <thead><tr>
-          <Th>name</Th>
-          <Th>nickname</Th>
-          <Th>alias</Th>
-          <Th>Modify</Th>
-          <Th>Delete</Th>
-        </tr></thead>
-        <tbody>
-          {lectures.map( (lecture) =>
-            <tr key={lecture.lecture.id}>
-              <Td>{lecture.lecture.name}</Td>
-              <Td><Input type="text" value={lecture.nickname}></Input></Td>
-              <Td><Input type="text" value={lecture.alias}></Input></Td>
-              <Td><Button>Modify</Button></Td>
-              <Td><Button>Delete</Button></Td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-      {children}
-    </Wrapper>
-  )
+class EditLectureTable extends React.Component {
+  constructor( props ){
+    super(props)
+    //this.lecture_id = -1;
+    this.modify = this.modify.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+  modify(author_id, lecture_id) {
+    console.log(author_id, lecture_id, register_nickname.value, register_alias.value);
+    //action_modify_lecture(author_id, lecture_id, register_nickname.value, register_alias.value)
+    this.forceUpdate()
+  }
+  delete(author_id, lecture_id) {
+    console.log(author_id, lecture_id);
+    //action_delete_lecture(author_id, lecture_id)
+    this.forceUpdate()
+  }
+  render(){
+    var {lectures, children, ...props} = this.props
+    console.log("hihiihihi", lectures);
+    return (
+      <Wrapper {...props}>
+        <Table>
+          <thead><tr>
+            <Th>name</Th>
+            <Th>nickname</Th>
+            <Th>alias</Th>
+            <Th>Modify</Th>
+            <Th>Delete</Th>
+          </tr></thead>
+          <tbody>
+            {lectures.map( (lecture) => {
+              var register_nickname, register_alias
+              return (
+                <tr key={lecture.lecture.id}>
+                  <Td>{lecture.lecture.name}</Td>
+                  <Td><Input type="text" value={lecture.nickname}></Input></Td>
+                  <Td><Input type="text" value={lecture.alias}></Input></Td>
+                  <Td><Button onClick={()=>this.modify(lecture.id, lecture.lecture.id)}>Modify</Button></Td>
+                  <Td><Button onClick={()=>this.delete(lecture.id, lecture.lecture.id)}>Delete</Button></Td>
+                </tr>
+              )
+            }
+            )}
+          </tbody>
+        </Table>
+        {children}
+      </Wrapper>
+    )
+  }
 }
 
 EditLectureTable.propTypes = {
--- frontend/src/components/molecules/EditLectureTable/index.test.js
+++ frontend/src/components/molecules/EditLectureTable/index.test.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Lecture from '.'
+import EditLectureTable from '.'
 
-const wrap = (props = {}) => shallow(<Lecture {...props} />)
-
-
-it('renders lectures when passed in', () => {
-  let lectures = [
-    {
-      code:"LECTURE_CODE",
-      credit:3,
-      id:1,
-      name:"LECTURE_NAME",
-      professor:"LECTURE_PROFESSOR",
-      semester:"LECTURE_SEMESTER"
-    }
-  ]
-  const wrapper = wrap({lectures: lectures})
-  expect(wrapper.contains('LECTURE_NAME')).toBe(true)
-  expect(wrapper.contains('LECTURE_SEMESTER')).toBe(true)
-  expect(wrapper.contains('LECTURE_PROFESSOR')).toBe(true)
-  expect(wrapper.contains('LECTURE_CODE')).toBe(true)
-})
+const wrap = (props = {}) => shallow(<EditLectureTable {...props} lectures={[]}/>)
 
 it('renders children when passed in', () => {
-  const wrapper = wrap({lectures: [], children: 'test' })
+  const wrapper = wrap({ children: 'test' })
   expect(wrapper.contains('test')).toBe(true)
 })
 
 it('renders props when passed in', () => {
-  const wrapper = wrap({lectures: [], id: 'foo' })
+  const wrapper = wrap({ id: 'foo' })
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
 })

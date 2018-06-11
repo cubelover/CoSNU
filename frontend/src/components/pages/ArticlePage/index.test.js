92c7240956cb7209cba1755b0f847ca30a6d55e1
kajebiii <kajebiii@gmail.com>
2018-06-11 19:23:06 +0900

    pass jest

--- frontend/src/components/pages/ArticlePage/index.test.js
+++ frontend/src/components/pages/ArticlePage/index.test.js
@@ -3,11 +3,16 @@ import { shallow } from 'enzyme'
 import ArticlePage from '.'
 
 it('renders', () => {
-  shallow(<ArticlePage params={{lecture_id: "1", article_id: "1"}} location={{query:{}}} article={{
-    id: 0,
-    title: "",
-    author: "",
-    create_time: "",
-    contents: ""
-}}/>)
+  shallow(<ArticlePage 
+    params={{lecture_id: "1", article_id: "1"}} 
+    location={{query:{}}} 
+    article={{
+      id: 0,
+      title: "",
+      author: "",
+      create_time: "",
+      contents: ""
+    }}
+    user_lectures={[]} 
+  />)  
 })

45ab6e9574289103831d2ff975dbc729f29af5c1
Sunghyeon Jo <ainta@naver.com>
2018-06-11 16:53:13 +0900

    fix typo

--- frontend/src/components/pages/WriteArticlePage/index.js
+++ frontend/src/components/pages/WriteArticlePage/index.js
@@ -18,7 +18,7 @@ const WriteArticlePage = ({params, action_post_article, children, ...props}) => 
       <Wrapper>
         <Input type="text" placeholder="title" innerRef={(ref) => {title = ref;}}></Input>
         <Link to={{ pathname: '/lecture/' + params.lecture_id + '/list/'}}><Button onClick={post_article}>POST</Button></Link>
-        <Link to={{ pathname: '/lecture/' + params.lecture_id + '/list/'}}><Button>Cancle</Button></Link>
+        <Link to={{ pathname: '/lecture/' + params.lecture_id + '/list/'}}><Button>Cancel</Button></Link>
       </Wrapper>
       <Textarea type="contents" placeholder="contents" innerRef={(ref) => {contents = ref;}}></Textarea>
       {children}
